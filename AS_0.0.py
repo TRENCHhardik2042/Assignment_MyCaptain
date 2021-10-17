e59f5e0ebbaf10e8080a4b65b3005ca18f745b77
Hardik Chemburkar <91974776+TRENCHhardik2042@users.noreply.github.com>
2021-10-18 00:09:26 +0530

    Delete AS_0.0.py

--- AS_0.0.py
+++
@@ -1,7 +0,0 @@
-String1 = ['10' , '1' , '8' , '3' , '6' , '5' , '4' , '7' , 'x' , 'y']
-print("The given Sequence is:")
-print(String1)
-index1 = String1.index('x')
-index2 = String1.index('y')
-print('The index of x is:', index1)
-print('The index of y is:', index2) 
